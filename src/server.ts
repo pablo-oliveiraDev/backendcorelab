89517942b0e2fdff25569fb177814af46b98dea6
Pablo Oliveira <pabloliverfe@gmail.com>
2024-01-31 01:33:33 -0300

    Fix(server.ts):Corrigido errocom alow origins

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,21 @@ import { router } from './routers/router';
 
 const app = express();
 const port = process.env.Port || 5080;
-app.use(cors());
+app.use(
+    cors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, // Se você estiver usando autenticação com credenciais (por exemplo, cookies)
+        optionsSuccessStatus: 204
+    })
+);
 app.use(
     express.urlencoded({
         extended: true
     })
 );
 app.use(express.json());
-//app.use(cors());
+
 app.use(router);
 app.listen(port, async () =>
     console.log(`Aplicação iniciada na porta ${port}!`)
