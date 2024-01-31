67a62666d8f7c59bfaa9ad5f23939a50f4ff3485
pablo-oliveiraDev <pabloliverfe@gmail.com>
2024-01-31 10:02:53 -0300

    teste liberação e rota

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,12 @@ import { router } from './routers/router';
 
 const app = express();
 const port = process.env.Port || 5080;
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // Se você estiver usando autenticação com credenciais (por exemplo, cookies)
-        optionsSuccessStatus: 204
-    })
-);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // ou substitua '*' pelo domínio específico do seu front-end
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(
     express.urlencoded({
         extended: true
