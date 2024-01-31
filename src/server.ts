24a8878a85c6f2d74d815e179580a9b0d7eb6510
pablo-oliveiraDev <pabloliverfe@gmail.com>
2024-01-31 12:11:53 -0300

    feat(next.config.js):deletado o arquivo5

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,17 @@ import { router } from './routers/router';
 
 const app = express();
 const port = process.env.Port || 5080;
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // ou substitua '*' pelo domínio específico do seu front-end
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    app.use(cors());
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE,PATCH, OPTIONS'
+    );
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
     next();
 });
 app.use(
